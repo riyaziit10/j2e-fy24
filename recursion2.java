9bcc6100ba13c4268a415562d6a0d5768477d1e1
Abhilasha2810 <121118499+Abhilasha2810@users.noreply.github.com>
2023-05-21 21:11:46 +0530

    Add files via upload

---
+++ recursion2.java
@@ -0,0 +1,30 @@
+package student.banasthali.abhilasha.recursion;
+
+
+  
+public class recursion2 {
+
+	static int numberOfPaths(int m, int n)
+	{
+	
+	
+		if (m == 1 || n == 1)
+			return 1;
+
+		
+		return numberOfPaths(m - 1, n)
+			+ numberOfPaths(m, n - 1);
+	
+	}
+
+	
+	public static void main(String args[])
+	{
+		System.out.println(numberOfPaths(3, 3));
+	}
+
+
+
+
+    
+}
